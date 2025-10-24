a9408395dbcb358b107c2c137f3901712770694b
Dhathri33 <dbathini@luc.edu>
2025-10-24 10:12:45 -0500

    Initial Commit

---
+++ Ghibli SwiftUI/Ghibli_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ghibli_SwiftUIApp.swift
+//  Ghibli SwiftUI
+//
+//  Created by Dhathri Bathini on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Ghibli_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
