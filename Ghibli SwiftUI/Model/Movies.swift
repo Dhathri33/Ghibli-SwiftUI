18fd333038aa90660e0510f1ed25c0a51c844be9
Dhathri33 <dbathini@luc.edu>
2025-10-24 12:14:01 -0500

    Added Model

---
+++ Ghibli SwiftUI/Model/Movies.swift
@@ -0,0 +1,17 @@
+//
+//  Movies.swift
+//  Ghibli SwiftUI
+//
+//  Created by Dhathri Bathini on 10/24/25.
+//
+struct Movies: Decodable {
+    var image: String?
+    var movie_banner: String?
+    var title: String?
+    var director: String?
+    var release_date: String?
+    var producer: String?
+    var running_time: String?
+    var rt_score: String?
+    var description: String?
+}
